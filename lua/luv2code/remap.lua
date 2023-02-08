eebe0237463ede2ba00ebcf1ee3858c2c317c324
Matthew Taylor <luv2code@gmail.com>
2023-02-08 10:57:46 -0600

    added quickfix mappings

--- lua/luv2code/remap.lua
+++ lua/luv2code/remap.lua
@@ -3,6 +3,10 @@ vim.keymap.set({"n", "v"}, "<leader>y", "\"+y") -- yank to clipboard
 vim.keymap.set({"n", "v"}, "<leader>p", "\"+p") -- paste from clipboard
 vim.keymap.set({"n", "v"}, "<leader>ep", vim.cmd.Ex) -- explorer project
 
+vim.keymap.set({"n", "v"}, "<leader>qq", ":ccl<cr>") -- quickfix keymaps
+vim.keymap.set({"n", "v"}, "<leader>qn", ":cn<cr>")
+vim.keymap.set({"n", "v"}, "<leader>qp", ":cp<cr>")
+
 vim.keymap.set("n", "<leader>ts", function () 
     -- tab small
     vim.opt.tabstop = 2
