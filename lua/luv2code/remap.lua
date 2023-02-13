5fd98927a3e82f9ed4225228982b5c30840789ae
Matthew Taylor <luv2code@gmail.com>
2023-02-13 14:20:04 -0600

    added navigation convenience bindings

--- lua/luv2code/remap.lua
+++ lua/luv2code/remap.lua
@@ -7,14 +7,28 @@ vim.keymap.set({"n", "v"}, "<leader>qq", ":ccl<cr>") -- quickfix keymaps
 vim.keymap.set({"n", "v"}, "<leader>qn", ":cn<cr>")
 vim.keymap.set({"n", "v"}, "<leader>qp", ":cp<cr>")
 
-vim.keymap.set("n", "<leader>ts", function () 
+--Window navigation
+vim.keymap.set({"n", "v"}, "<C-k>", "<C-w>k")
+vim.keymap.set({"n", "v"}, "<C-j>", "<C-w>j")
+vim.keymap.set({"n", "v"}, "<C-l>", "<C-w>l")
+vim.keymap.set({"n", "v"}, "<C-h>", "<C-w>h")
+
+-- Buffer navigation
+vim.keymap.set({"n", "v"}, "<leader>k", ":bnext<cr>")
+vim.keymap.set({"n", "v"}, "<leader>j", ":bprevious<cr>")
+
+-- Tab navigation
+vim.keymap.set({"n", "v"}, "<leader>l", ":tabnext<cr>")
+vim.keymap.set({"n", "v"}, "<leader>h", ":tabprevious<cr>")
+
+vim.keymap.set("n", "<leader>ts", function ()
     -- tab small
     vim.opt.tabstop = 2
     vim.opt.softtabstop = 2
     vim.opt.shiftwidth = 2
     vim.opt.expandtab = true
 end)
-vim.keymap.set("n", "<leader>tl", function () 
+vim.keymap.set("n", "<leader>tl", function ()
     -- tab large
     vim.opt.tabstop = 4
     vim.opt.softtabstop = 4
