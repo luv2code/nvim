69f450dd622b700699d654ad664b23e14a05ef83
Matthew Taylor <luv2code@gmail.com>
2023-04-03 15:42:23 -0500

    fixed issue with omnisharp tokens

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -11,4 +11,14 @@ lsp.ensure_installed({
   'cssls',
 })
 lsp.nvim_workspace()
+
+lsp.on_attach(function(client)
+  if client.name == "omnisharp" then
+    client.server_capabilities.semanticTokensProvider.legend = {
+      tokenModifiers = { "static" },
+      tokenTypes = { "comment", "excluded", "identifier", "keyword", "keyword", "number", "operator", "operator", "preprocessor", "string", "whitespace", "text", "static", "preprocessor", "punctuation", "string", "string", "class", "delegate", "enum", "interface", "module", "struct", "typeParameter", "field", "enumMember", "constant", "local", "parameter", "method", "method", "property", "event", "namespace", "label", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "regexp", "regexp", "regexp", "regexp", "regexp", "regexp", "regexp", "regexp", "regexp" }
+    }
+  end
+end)
+
 lsp.setup()
