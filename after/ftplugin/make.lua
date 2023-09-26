73e5a76ad86fdee4953a36feb55b8ba64efbb752
Matthew Taylor <luv2code@gmail.com>
2023-09-25 21:23:38 -0500

    added after/ftplugin/make.lua

---
+++ after/ftplugin/make.lua
@@ -0,0 +1,7 @@
+local setlocal = vim.opt_local
+-- Tabs and spaces
+setlocal.expandtab = false
+setlocal.smartindent = false
+setlocal.tabstop = 4
+setlocal.shiftwidth = 4
+setlocal.softtabstop = 4
--- lua/luv2code/init.lua
+++ lua/luv2code/init.lua
@@ -1,3 +1,3 @@
 require("luv2code.remap")
-require("luv2code.packer")
 require("luv2code.set")
+require("luv2code.packer")
