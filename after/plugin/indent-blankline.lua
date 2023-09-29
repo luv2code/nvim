0d5359292c33b0468794e55c7e8f2eb68a619c14
Matthew Taylor <luv2code@gmail.com>
2023-09-28 22:29:18 -0500

    updated indent-blankline to v3 config

--- after/plugin/indent-blankline.lua
+++ after/plugin/indent-blankline.lua
@@ -1,9 +1,8 @@
-vim.opt.list = true
---vim.opt.listchars:append "space:⋅"
---vim.opt.listchars:append "eol:↴"
-
-require("indent_blankline").setup {
-    --space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true,
-}
+require("ibl").setup({
+	config = {
+		scope = {
+			enabled = true,
+			show_start = true,
+		},
+	}
+})
