af23c2fad4f2d31a9e82bb805f658cbc3143b07e
Matthew Taylor <luv2code@gmail.com>
2023-09-29 11:08:43 -0500

    add UpdateAll vim cmd to update everything.

--- init.lua
+++ init.lua
@@ -1,1 +1,22 @@
 require("luv2code")
+
+function UpdateEverything()
+    -- Update Neovim Plugins
+    vim.cmd('PackerSync')
+
+    -- Update Mason plugins
+    vim.cmd('MasonUpdate')
+
+    -- Update Go Tool
+    vim.cmd('GoUpdateBinaries')
+
+    -- Update TreeSitter plugins
+    vim.cmd('TSUpdate')
+
+    -- Restart NeoVim to make sure all the updates take effect
+    vim.cmd('silent exec "!kill -s HUP $PPID"')
+end
+
+vim.cmd([[
+  command! UpdateAll lua UpdateEverything()
+]])
