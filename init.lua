d162c36be7d46a6cdc647397519a222c5018b00d
Matthew Taylor <luv2code@gmail.com>
2024-04-28 20:03:02 -0500

    re-arranged update function

--- init.lua
+++ init.lua
@@ -1,22 +1,28 @@
 require("luv2code")
 
 function UpdateEverything()
-    -- Update Neovim Plugins
-    vim.cmd('PackerSync')
-
     -- Update Mason plugins
     vim.cmd('MasonUpdate')
+
+    -- Update Neovim Plugins
+    vim.cmd('PackerSync')
 
     -- Update Go Tool
     vim.cmd('GoUpdateBinaries')
 
     -- Update TreeSitter plugins
     vim.cmd('TSUpdate')
+end
 
+function Restart()
     -- Restart NeoVim to make sure all the updates take effect
     vim.cmd('silent exec "!kill -s HUP $PPID"')
 end
 
 vim.cmd([[
   command! UpdateAll lua UpdateEverything()
+]])
+
+vim.cmd([[
+  command! Restart lua Restart()
 ]])
