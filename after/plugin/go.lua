3e58493b90bb2a28ed5a2c8379c1c71a7730848e
Matthew Taylor <luv2code@gmail.com>
2024-07-02 17:10:58 -0500

    enable templ format on save

--- after/plugin/go.lua
+++ after/plugin/go.lua
@@ -17,21 +17,21 @@ vim.cmd(
 	[[command! GoLintEx        :setl makeprg=golangci-lint\ run\ --print-issued-lines=false\ --exclude-use-default=true\ --out-format=line-number | :GoMake]]
 )
 
--- vim.api.nvim_create_autocmd("BufWritePre", {
--- 	pattern = { "*.templ" },
--- 	callback = function()
--- 		local bufnr = vim.api.nvim_get_current_buf()
--- 		local filename = vim.api.nvim_buf_get_name(bufnr)
--- 		local cmd = "templ fmt " .. vim.fn.shellescape(filename)
---
--- 		vim.fn.jobstart(cmd, {
--- 				on_exit = function()
--- 						-- Reload the buffer only if it's still the current buffer
--- 						if vim.api.nvim_get_current_buf() == bufnr then
--- 								vim.cmd('e!')
--- 						end
--- 				end,
--- 		})
--- 	end,
---   group = format_sync_grp,
--- })
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.templ" },
+	callback = function()
+		local bufnr = vim.api.nvim_get_current_buf()
+		local filename = vim.api.nvim_buf_get_name(bufnr)
+		local cmd = "templ fmt " .. vim.fn.shellescape(filename)
+
+		vim.fn.jobstart(cmd, {
+				on_exit = function()
+						-- Reload the buffer only if it's still the current buffer
+						if vim.api.nvim_get_current_buf() == bufnr then
+								vim.cmd('e!')
+						end
+				end,
+		})
+	end,
+  group = format_sync_grp,
+})
