409e96ad5cfd9ef1a585497d5dff8ef40935a611
Matthew Taylor <luv2code@gmail.com>
2024-07-04 15:23:57 -0500

    fix indenting in lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -74,15 +74,15 @@ return {
 			}
 		})
 
-			require("luasnip.loaders.from_vscode").lazy_load()
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-			local has_words_before = function()
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-			end
-			local luasnip = require("luasnip")
+		local has_words_before = function()
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+		local luasnip = require("luasnip")
 
-			cmp.setup({
+		cmp.setup({
 			mapping = {
 				["<Enter>"] = function(fallback)
 					-- Don't block <CR> if signature help is active
