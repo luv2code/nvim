65d1e909995b6dbc6c0402ebb422b129bf2aad10
Matthew Taylor <luv2code@gmail.com>
2024-07-05 12:06:23 -0500

    add yaml to treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
             ensure_installed = {
                 "vimdoc", "javascript", "typescript", "c", "lua", "rust",
                 "jsdoc", "bash", "go", "html", "markdown_inline", "markdown",
+								"yaml",
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
