6e9ca924de728951815711b3cb9e0f364f70d729
Matthew Taylor <luv2code@gmail.com>
2024-07-04 21:43:35 -0500

    fix autocomplete mapping

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # Prerequisite
 
-clone the packer repo
-```bash
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-```
-
-Then open nvim and run `:PackerSync`
-
 Install [nerd fonts](https://www.nerdfonts.com/font-downloads) for nvim-dap-ui. I prefer [JetBrains Mono](./fonts/JetBrainsMonoNerdFont.7z).
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -81,6 +81,26 @@ return {
 			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 		end
 		local luasnip = require("luasnip")
+		local next_cmp = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" })
+		local prev_cmp = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" })
 
 		cmp.setup({
 			mapping = {
@@ -115,28 +135,10 @@ return {
 						end
 					end,
 				}),
-
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					elseif has_words_before() then
-						cmp.complete()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
+				["<Tab>"] = next_cmp,
+				["<C-n>"] = next_cmp,
+				["<S-Tab>"] = prev_cmp,
+				["<C-p>"] = prev_cmp,
 			},
 
 			sources = {
