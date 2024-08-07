82baf907c8a93a2edcf532d386b3734ce8fdb2ee
Matthew Taylor <luv2code@gmail.com>
2024-08-07 15:02:03 -0500

    update markview theme

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -2,8 +2,12 @@ return {
 	"OXY2DEV/markview.nvim",
 	config = function()
 		require("markview").setup({
-			highlight_groups = vim.list_extend(require("markview").configuration.highlight_groups, require ("markview.presets").heading.simple_hls),
-			headings = require("markview.presets").heading.simple_better
+			-- highlight_groups = vim.list_extend(require("markview").configuration.highlight_groups, require ("markview.presets").heading.simple_hls),
+			-- headings = require("markview.presets").heading.simple_better
+			code_blocks = {
+					hl = "CursorLine",
+					position = nil,
+			},
 		})
 	end,
 	branch = "dev",
