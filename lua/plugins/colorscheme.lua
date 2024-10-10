9c3e7759ed42fc4ea0cb054d38a011fe2a53211f
Matthew Taylor <luv2code@gmail.com>
2024-10-10 16:30:39 -0500

    fix colorscheme for markview md headings

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,8 +7,6 @@ return {
 			vim.g.minimal_italic_comments = true
 			vim.g.minimal_transparent_background = true
 			vim.cmd[[colorscheme minimal-base16]]
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		end,
 	}
 }
--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,16 +1,8 @@
 return {
 	"OXY2DEV/markview.nvim",
 	config = function()
-		require("markview").setup({
-			-- highlight_groups = vim.list_extend(require("markview").configuration.highlight_groups, require ("markview.presets").heading.simple_hls),
-			-- headings = require("markview.presets").heading.simple_better
-			code_blocks = {
-					hl = "CursorLine",
-					position = nil,
-			},
-		})
+		require("markview").setup({})
 	end,
-	-- branch = "dev",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"nvim-tree/nvim-web-devicons"
