cb4a56784972d4e8e1cd9c03845308232e93169c
Matthew Taylor <luv2code@gmail.com>
2024-10-22 16:43:13 -0500

    Add undercurl to diagnostic errors and misspellings

---
+++ lua/config/highlight.lua
@@ -0,0 +1,3 @@
+-- hi SpellBad gui=undercurl
+vim.api.nvim_set_hl(0, "SpellBad", { undercurl = true, fg = "#cc6674", sp = "NvimLightRed" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", {undercurl = true, sp = "NvimLightRed" })
