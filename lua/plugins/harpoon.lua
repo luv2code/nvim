ed4818f3790bf024195f36dd201d0a171916c865
Matthew Taylor <luv2code@gmail.com>
2024-11-03 19:10:35 -0600

    add descriptions to harpoon keymaps

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,16 +9,16 @@ return {
 		harpoon:setup()
 		-- REQUIRED
 
-		vim.keymap.set("n", "<leader>lx", function() harpoon:list():add() end)
-		vim.keymap.set("n", "<leader>ll", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+		vim.keymap.set("n", "<leader>lx", function() harpoon:list():add() end, { desc = "Add mark (Harpoon)" })
+		vim.keymap.set("n", "<leader>ll", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Open menu (Harpoon)" })
 
-		vim.keymap.set("n", "<leader>la", function() harpoon:list():select(1) end)
-		vim.keymap.set("n", "<leader>ls", function() harpoon:list():select(2) end)
-		vim.keymap.set("n", "<leader>ld", function() harpoon:list():select(3) end)
-		vim.keymap.set("n", "<leader>lf", function() harpoon:list():select(4) end)
+		vim.keymap.set("n", "<leader>la", function() harpoon:list():select(1) end, { desc = "Open mark 1 (Harpoon)" })
+		vim.keymap.set("n", "<leader>ls", function() harpoon:list():select(2) end, { desc = "Open mark 2 (Harpoon)" })
+		vim.keymap.set("n", "<leader>ld", function() harpoon:list():select(3) end, { desc = "Open mark 3 (Harpoon)" })
+		vim.keymap.set("n", "<leader>lf", function() harpoon:list():select(4) end, { desc = "Open mark 4 (Harpoon)" })
 
 		-- Toggle previous & next buffers stored within Harpoon list
-		vim.keymap.set("n", "<leader>lp", function() harpoon:list():prev() end)
-		vim.keymap.set("n", "<leader>ln", function() harpoon:list():next() end)
+		vim.keymap.set("n", "<leader>lp", function() harpoon:list():prev() end, { desc = "Previous mark (Harpoon)" })
+		vim.keymap.set("n", "<leader>ln", function() harpoon:list():next() end, { desc = "Next mark (Harpoon)" })
 	end,
 }
