797ec5a240923c2b2bcbe5ee88df609bad081b5a
Matthew Taylor <luv2code@gmail.com>
2025-02-12 14:00:14 -0600

    fix go format on save to use fumpt

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -10,6 +10,7 @@ return {
   config = function()
     require("go").setup({
 			icons = {breakpoint = '🛑', currentpos = ''},
+			lsp_gofumpt = true,
 		})
 
 		local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
@@ -17,7 +18,7 @@ return {
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			pattern = "*.go",
 			callback = function()
-			 require('go.format').goimport()
+			  vim.lsp.buf.format()
 			end,
 			group = format_sync_grp,
 		})
