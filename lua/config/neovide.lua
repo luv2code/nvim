3c2ef2e51a75ddff4bc202a69a04b70fa37f1abb
Matthew Taylor <luv2code@gmail.com>
2025-04-21 19:53:46 -0500

    change a depreated neovide setting

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -2,5 +2,5 @@ if vim.g.neovide then
 	vim.g.neovide_cursor_animation_length = 0.01
 	vim.g.neovide_cursor_trail_size = 0.7
 	vim.o.guifont = "JetBrainsMono Nerd Font Mono:h16"
-	vim.g.neovide_transparency = 0.9
+	vim.g.neovide_opacity = 0.9
 end
