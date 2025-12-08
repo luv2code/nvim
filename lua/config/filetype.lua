3ca4028018bb0fb5034615f8be8cebe5120eb693
Matthew Taylor <luv2code@gmail.com>
2025-12-08 16:35:40 -0600

    Modify custom shebang filetype detection to use file extension first.

--- lua/config/filetype.lua
+++ lua/config/filetype.lua
@@ -2,17 +2,20 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWrite" }, {
   group = vim.api.nvim_create_augroup("CustomShebangDetection", {}),
   desc = "Set the filetype based on the shebang header",
   callback = function()
-    local line = vim.fn.getline(1)
-    local pattern3, pattern1, pattern2 = "^#!.*/bin/env%s+-S%s+(%w+)", "^#!.*/bin/env%s+(%w+)", "^#!.*/bin/(%w+)"
-    local interpreter = line:match(pattern3) or line:match(pattern1) or line:match(pattern2)
+		if vim.fn.expand("%:e") ~= "" then -- use file extension if it exists
+			return
+		end
+		local line = vim.fn.getline(1)
+		local pattern3, pattern1, pattern2 = "^#!.*/bin/env%s+-S%s+(%w+)", "^#!.*/bin/env%s+(%w+)", "^#!.*/bin/(%w+)"
+		local interpreter = line:match(pattern3) or line:match(pattern1) or line:match(pattern2)
 		if interpreter == "bun" then
 			interpreter = "typescript"
 		end
 		if interpreter == "uv" then
 			interpreter = "python"
 		end
-    if interpreter then
-      vim.api.nvim_set_option_value("filetype", interpreter, { buf = 0 })
-    end
+		if interpreter then
+			vim.api.nvim_set_option_value("filetype", interpreter, { buf = 0 })
+		end
   end,
 })
